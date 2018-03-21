eeabcfa63b0fb1a62c7388acaa845679d73a4f23
Sumaira Memon <sumairamemon@dhcp-v116-091.mobile.uci.edu>
2018-03-20 17:13:19 -0700

    updates

---
+++ Solutions/keys.js
@@ -0,0 +1,15 @@
+console.log("Keys loaded");
+
+exports.twitter = {
+  consumer_key: process.env. AIzaSyDFkntugH-pXh5GT9tCLj10gqX1yNgC6z0,
+  consumer_secret: process.env. AIzaSyDFkntugH,
+  access_token_key: process.env.9tCLj10gqX1y,
+  access_token_secret: process.env.9tCLj10gqX1y
+};
+
+
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
